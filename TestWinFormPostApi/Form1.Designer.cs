3d41d554991a299cc6368d338a607dcd6e8b0eed
surajExi <suraj.0241@gmail.com>
2016-04-16 01:21:58 +0530

    Check In
    Check In

---
+++ TestWinFormPostApi/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TestWinFormPostApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput2 = new System.Windows.Forms.TextBox();
+            this.txtOutput3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Post API";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(121, 55);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(244, 20);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtOutput2
+            // 
+            this.txtOutput2.Location = new System.Drawing.Point(121, 81);
+            this.txtOutput2.Name = "txtOutput2";
+            this.txtOutput2.Size = new System.Drawing.Size(244, 20);
+            this.txtOutput2.TabIndex = 5;
+            // 
+            // txtOutput3
+            // 
+            this.txtOutput3.Location = new System.Drawing.Point(121, 107);
+            this.txtOutput3.Name = "txtOutput3";
+            this.txtOutput3.Size = new System.Drawing.Size(244, 20);
+            this.txtOutput3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 262);
+            this.Controls.Add(this.txtOutput3);
+            this.Controls.Add(this.txtOutput2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput2;
+        private System.Windows.Forms.TextBox txtOutput3;
+    }
+}
+
---
+++ TestWinFormPostApi/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWinFormPostApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private SerialPort mySerialPort = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SerialPort mySerialPort = new SerialPort();
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            mySerialPort.Open();
+            string value = "After Test Post API (Result From API)"; //This value is sent to the API and again returned the same from it
+            if ((mySerialPort.ReadExisting()) == String.Empty) txtOutput2.Text = "Testing Port";
+            var text = DataReceivedHandler(value);
+            txtOutput.Text = text.Result; 
+            mySerialPort.Close();
+        }
+
+        /// <summary>
+        /// Event Handler Method Add if required
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            //if ((mySerialPort.ReadByte()) == 0) txtOutput2.Text = "Testing Port";
+        }
+
+        private static async Task<string> DataReceivedHandler(string value)
+        {
+            using (var client = new HttpClient())
+            {
+                var url = new Uri("http://localhost:58941/api/values?value="); //Change the Url to the Host (This is the default Values APi Controller)
+                var result = client.PostAsJsonAsync(url, value);
+                result.Result.EnsureSuccessStatusCode();
+                if (result.Result.IsSuccessStatusCode)
+                {
+                    string testResult = await result.Result.Content.ReadAsStringAsync();
+                    return testResult;
+                }
+                return await Task.Delay(10000)
+                                .ContinueWith(t => "Hello");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
